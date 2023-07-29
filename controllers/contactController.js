1385677ff1c3607f9582982956805b6fc18b7356
Jenil Vora <jenil.vora0408@gmail.com>
2023-07-29 20:15:42 +0530

    Created Controller Methods for CRUD & Error Handler

---
+++ controllers/contactController.js
@@ -0,0 +1,48 @@
+//@desc - Get all Contacts
+//@route - GET /api/contacts
+//@access - Public
+const getContacts = async (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+};
+
+//@desc - Create new Contact
+//@route - POST /api/contacts
+//@access - Public
+const createContact = async (req, res) => {
+  console.log("The request body is: ", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+  res.status(201).json({ message: "Create Contact" });
+};
+
+//@desc - Get Contact by ID
+//@route - GET /api/contacts/:id
+//@access - Public
+const getContact = async (req, res) => {
+  res.status(200).json({ message: `Get contact for ${req.params.id} ` });
+};
+
+//@desc - Update Contact
+//@route - PUT /api/contacts/:id
+//@access - Public
+const updateContact = async (req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id} ` });
+};
+
+//@desc - Delete Contact
+//@route - DELETE /api/contacts/:id
+//@access - Public
+const deleteContact = async (req, res) => {
+  res.status(200).json({ message: `Delete contact for ${req.params.id} ` });
+};
+
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
